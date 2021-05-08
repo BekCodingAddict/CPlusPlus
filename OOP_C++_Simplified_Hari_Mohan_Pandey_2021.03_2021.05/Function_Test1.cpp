4499ac63c4e02810e3219efeb9efab574b340f07
Otabek <70604119+Optimus970803@users.noreply.github.com>
2021-05-09 05:28:49 +0900

    Add files via upload

---
+++ OOP_C++_Simplified_Hari_Mohan_Pandey_2021.03_2021.05/Function_Test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Head.hh"
+int main() {
+	Function f1;
+	int result = f1.Factorial(5);
+	std::cout << "Result >>" << result;
+	std::cin.get();
+}
---
+++ OOP_C++_Simplified_Hari_Mohan_Pandey_2021.03_2021.05/Header1.hh
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef Header.hh
+#define Header.hh
+int max(int num1, int num2) { return num1 > num2 ? num1 : num2; }
+#endif
