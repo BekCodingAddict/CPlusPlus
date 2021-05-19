64d0a757f9c2a5c94b80c93f79addb3aed160ba9
Otabek <70604119+Optimus970803@users.noreply.github.com>
2021-05-20 07:37:32 +0900

    Delete OOP_C++_Simplified_Hari_Mohan_Pandey_2021.03_2021.05 directory

--- OOP_C++_Simplified_Hari_Mohan_Pandey_2021.03_2021.05/Function_Test1_Main_of_Head.hh_.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Head.hh"
-int main() {
-	Function f1;
-	int result = f1.Factorial(5);
-	std::cout << "Result >>" << result;
-	std::cin.get();
-}
--- OOP_C++_Simplified_Hari_Mohan_Pandey_2021.03_2021.05/Source1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Using_Operator_Overloading.h"
-
-int main() {
-
-	return 0;
-}//380 page
